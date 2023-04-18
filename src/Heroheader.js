cc894d8c26b00974de48f8b4ab1fca86a6c8c1e9
nauticalstars <nauticalstars21@gmail.com>
2023-04-17 20:13:25 -0700

    initial commit

---
+++ src/Heroheader.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Heroheader.css";
+
+export default function Heroheader() {
+  <section class="bgimage" id="home">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 hero-text">
+          <h2 class="hero_title">Hi, I'm Daria</h2>
+          <p class="hero_desc">
+            I am an IT consultant and junior Web Developper in Whistler, BC
+            Canada
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>;
+}
